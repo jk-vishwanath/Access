2575715f1e8b82a08717d843c31315c881e33f1c
jk-vishwanath <66640951+jk-vishwanath@users.noreply.github.com>
2025-03-28 23:04:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,19 @@ funding_stage = st.selectbox("Funding Stage", ["Pre-Seed", "Seed", "Round A+", "
 # Create a dictionary for one-hot encoding
 input_data = {
     "Classification_CXO/ Founder": classification == "CXO/ Founder",
+    "Classification_Investors": classification == "Investors",
+    "Classification_Researcher": classification == "Research", 
+    "Classification_Student": classification == "Student", 
+    "Classification_Other": classification == "Other", 
+    "Industry_Healthcare": industry == "Healthcare", 
+    "Industry_Finance": industry == "Finance", 
+    "Industry_Other": == "Other", 
     "Industry_Technology & AI": industry == "Technology & AI",
     "funding_stage_filled_Round A+": funding_stage == "Round A+"
+    "funding_stage_filled_Pre-Seed": funding_stage == "Pre-Seed"
+    "funding_stage_filled_Seed": funding_stage == "Seed"
+    "funding_stage_filled_Other": funding_stage == "Other"
+
 }
 
 # Convert to DataFrame
